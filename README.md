5c30bf6034ccaf14f92fab87d1313b7099f9ad0a
justinrosales <jpnrosales@outlook.com>
2022-10-11 18:01:45 -0700

    hello

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # I have no clue what I am doing, but one day I will figure it all out
 # One day this intro file with be balling just wait - 9/23/2022
 # WTF WHY IS EVERYTHING SO BIG?!
+
+# update: IT WORKED, IT FINALLY WORKED
+# im becoming a programming prodigy day by day 
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-print ("Hello World! I am still trying to figure out how to connect VS Code to Github")
+print("Hello World! I am still trying to figure out how to connect VS Code to Github")
 
-# when I send a push request it doesn't update on github for some reason 
-# pain. 
+# when I send a push request it doesn't update on github for some reason
+# IT FINALLY WORKED
+
+# 9/26/22 - hmmm.. when I try do git push -u origin master, it doesn't update on github
+# on the terminal it says "Everything up-to-date" even though that is not the case :/
